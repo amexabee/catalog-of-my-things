052321f5ed5a1e42db509d14af1619a5cad47006
Martin Perez Bazan <martin.perezbazan@gmail.com>
2023-02-13 19:03:18 -0300

    Add properties to Item class

--- item.rb
+++ item.rb
@@ -1,2 +1,25 @@
 class Item
+  attr_reader :id, :archived
+
+  def initialize(publish_date, archived: false)
+    @id = Random.rand(1...1000)
+    @publish_date = publish_date
+    @archived = archived
+  end
+
+  def author(author)
+    @author = author
+  end
+
+  def genre(genre)
+    @genre = genre
+  end
+
+  def source(source)
+    @source = source
+  end
+
+  def label(label)
+    @label = label
+  end
 end
