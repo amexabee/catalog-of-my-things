e59894db9fec849187cad8f91ffef3bf850732c9
amexabee <amexabee@yahoo.com>
2023-02-14 23:08:37 +0200

    Add a test for can_be_archived? method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -18,7 +18,11 @@ describe Game do
     end
 
     it 'Return the correct published date' do
-        expect(@game.publish_date).to eql '2023-02-13'
+      expect(@game.publish_date).to eql '2023-02-13'
+    end
+
+    it 'Returns whether game can be archived or not' do
+      expect(@game.can_be_archived?).to eql false
     end
   end
 end
