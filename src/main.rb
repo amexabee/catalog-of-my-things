d72313771fd8d556a2f89c6301d592920b606542
amexabee <amexabee@yahoo.com>
2023-02-14 17:17:20 +0200

    Apply reviewers comment

--- src/main.rb
+++ src/main.rb
@@ -3,14 +3,20 @@ def main
 
   loop do
     puts "\nPlease choose an option by entering a number
-    1. Get genre
-    2. Get author
-    3. Get source
-    4. Get label
-    5. Get published date
-    6. Exit"
-    input = gets.chomp.to_i
-    if input == 6
+    1. List all books
+    2. List all music albums
+    3. List all movies
+    4. List of games
+    5. List all genres (e.g 'Comedy', 'Thriller')
+    6. List all labels (e.g. 'Gift', 'New')
+    7. List all authors (e.g. 'Stephen King')
+    8. List all sources (e.g. 'From a friend', 'Online shop')
+    9. Add a book
+    10. Add a music album
+    11. Add a movie
+    12. Add a game"
+
+    if gets.chomp.to_i == 6
       puts "\nGood Bye."
       break
     end
