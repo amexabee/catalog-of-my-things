fa25170cadd29e11a7e3f4a64359328c3e32bdae
Martin Perez Bazan <martin.perezbazan@gmail.com>
2023-02-14 17:35:18 -0300

    update add_item method in label class

--- src/label.rb
+++ src/label.rb
@@ -1,4 +1,6 @@
 class Label
+  attr_accessor :title, :color, :items
+
   def initialize(title, color)
     @id = Random.rand(1...1000)
     @title = title
@@ -8,6 +10,6 @@ class Label
 
   def add_item(item)
     @items.push(item)
-    item.label = self
+    item.add_label(self)
   end
 end
