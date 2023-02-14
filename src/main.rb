b33678d2c97f4d597f4bae71e74c9464a4fa3d46
amexabee <amexabee@yahoo.com>
2023-02-14 16:30:10 +0200

    Implement startup actions

--- src/main.rb
+++ src/main.rb
@@ -1,4 +1,19 @@
 def main
   puts 'Welcome to My Catalog of Things'
+
+  loop do
+    puts "\nPlease choose an option by entering a number
+    1. Get genre
+    2. Get author
+    3. Get source
+    4. Get label
+    5. Get published date
+    6. Exit"
+    input = gets.chomp.to_i
+    if input == 6
+      puts "\nGood Bye."
+      break
+    end
+  end
 end
 main
