98f64ed482cf572adb90099665bdff646a55bab8
Martin Perez Bazan <martin.perezbazan@gmail.com>
2023-02-15 19:01:17 -0300

    Refactor assign label to select previous labels

--- src/functions/assign_label.rb
+++ src/functions/assign_label.rb
@@ -1,4 +1,7 @@
 require_relative '../label'
+require_relative './show_books_items'
+require_relative './show_games_items'
+require_relative './show_music_items'
 
 ITEMS_TYPE = %w[BOOK MUSIC GAME MOVIE].freeze
 def assign_label
@@ -11,68 +14,46 @@ def assign_label
     item_type = gets.chomp.to_i
     break if item_type.zero?
 
-    show_books if item_type == 1
-    show_music if item_type == 2
-    show_games if item_type == 3
+    case item_type
+    when 1
+      selected_index = show_books_items
+      assign(books[selected_index])
+    when 2
+      show_music_items
+    when 3
+      show_games_items
+    else
+      puts 'Select a correct type'
+    end
   end
 end
 
-def show_music
-  if music.empty?
-    puts "No music in catalog\n\n"
-    return
+def select_label
+  puts "\nSelect the label to assign or create a new one:"
+  labels.each_with_index do |label, index|
+    puts "#{index + 1}) #{label.title} - Color: #{label.color}"
   end
-  puts 'Select the music to assign label by index number'
-  music.each_with_index do |music, index|
-    puts "#{index}) #{music.publish_date} - #{music.on_spotify}"
+  puts '0) CREATE NEW LABEL'
+  select_input = gets.chomp.to_i
+  if select_input.zero?
+    create_label
+  else
+    labels[select_input - 1]
   end
-  selected_index = gets.chomp.to_i
+end
+
+def create_label
   print 'Label title: '
   title = gets.chomp
   print 'Label color: '
   color = gets.chomp
   label = Label.new(title, color)
-  music[selected_index].add_label(label)
   labels << label
-  puts 'Label assigned successfully'
+  label
 end
 
-def show_books
-  if books.empty?
-    puts "No books in catalog\n\n"
-    return
-  end
-  puts 'Select the book to assign label by index number'
-  books.each_with_index do |book, index|
-    puts "#{index}) #{book.publisher} - #{book.publish_date} - #{book.cover_state}"
-  end
-  selected_index = gets.chomp.to_i
-  print 'Label title: '
-  title = gets.chomp
-  print 'Label color: '
-  color = gets.chomp
-  label = Label.new(title, color)
-  books[selected_index].add_label(label)
-  labels << label
-  puts 'Label assigned successfully'
-end
-
-def show_games
-  if games.empty?
-    puts "No games in catalog\n\n"
-    return
-  end
-  puts 'Select the game to assign label by index number'
-  games.each_with_index do |game, index|
-    puts "#{index}) #{game.multiplayer} - #{game.publish_date}"
-  end
-  selected_index = gets.chomp.to_i
-  print 'Label title: '
-  title = gets.chomp
-  print 'Label color: '
-  color = gets.chomp
-  label = Label.new(title, color)
-  games[selected_index].add_label(label)
-  labels << label
+def assign(item)
+  label = select_label
+  item.add_label(label)
   puts 'Label assigned successfully'
 end
