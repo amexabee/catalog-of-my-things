9e0894a81286e2066785af8c7ea6790ef3a17545
Martin Perez Bazan <martin.perezbazan@gmail.com>
2023-02-15 19:03:29 -0300

    Fix new line in list books

--- src/functions/list_books.rb
+++ src/functions/list_books.rb
@@ -3,7 +3,6 @@ def list_books
   puts 'No books in the catalog' if books.empty?
   books.each do |element|
     print "ID: #{element.id} - Publisher: #{element.publisher} - Cover State: #{element.cover_state}"
-    print " - Publish Date: #{element.publish_date}"
+    print " - Publish Date: #{element.publish_date}\n"
   end
-  print "\n"
 end
